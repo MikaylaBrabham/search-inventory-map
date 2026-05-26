dece656a9c5f00261338c7f556801b66e6a9b59c
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-25 20:59:30 -0500

    added product plan copy and paste from last array - variables - constructors - get and set

--- src/main/java/com.pluralsight/Product.java
+++ src/main/java/com.pluralsight/Product.java
@@ -3,4 +3,31 @@ package com.pluralsight;
 public class Product {
 
     //add product to this project from last array project
+
+    private int id;
+    private String name;
+    private float price;
+
+    // add product format constructor
+
+
+    public Product(int id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    //add getters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
 }
