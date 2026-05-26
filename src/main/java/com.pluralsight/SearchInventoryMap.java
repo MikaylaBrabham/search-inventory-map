ba11b10d1d7c145a0bfb46ac826f5df3696ded80
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-25 20:35:19 -0500

    added loadinventory buff reader and plan to format user input

--- src/main/java/com.pluralsight/SearchInventoryMap.java
+++ src/main/java/com.pluralsight/SearchInventoryMap.java
@@ -1,6 +1,7 @@
 package com.pluralsight;
 
 //add imports
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
@@ -10,24 +11,24 @@ import java.util.Scanner;
 public class SearchInventoryMap {
 
     //add hashmaps
-static HashMap<String, Product> inventory = new HashMap<>();
+    static HashMap<String, Product> inventory = new HashMap<>();
     //add scanner
     Scanner myScanner = new Scanner(System.in);
 
     //add main
     public static void main(String[] args) {
 
-    //add loadinventory
+        //add loadinventory
         loadInventory();
 
-    //add while true
+        //add while true
         while (true) {
             System.out.print("Enter product name or X to exit: ");
             String search = myScanner.nextLine();
 
 
             //add if statement
-            if(search.equalsIgnoreCase("X"));
+            if (search.equalsIgnoreCase("X")) ;
             break;
         }
 
@@ -37,5 +38,23 @@ static HashMap<String, Product> inventory = new HashMap<>();
         //add if statement for product
         if (product == null) {
             System.out.println("product not found.");
+        } else {
+            System.out.println("ID: %d Name: %s Price $%.2f%n",
+                    product.getId(),
+                    product.getName(),
+                    product.getPrice());
         }
+    }
+
+//add load inventory
+public static void loadInventory() {
+
+//add loadinventory try/ catch buff reader and file reader
+try {
+BufferedReader reader = new BufferedReader(new FileReader("inventory.csv"));
+
+//create string input
+    String input;
+
+    //add while statement to format and parse input
 }
