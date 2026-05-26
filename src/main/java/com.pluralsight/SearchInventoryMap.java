aff109dc103061393dc27b5ccf282feb4116c6d3
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-25 20:17:55 -0500

    added imports, hashmap, scanner

--- src/main/java/com.pluralsight/SearchInventoryMap.java
+++ src/main/java/com.pluralsight/SearchInventoryMap.java
@@ -1,15 +1,23 @@
 package com.pluralsight;
 
 //add imports
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Scanner;
+
 
 public class SearchInventoryMap {
 
     //add hashmaps
-
+static HashMap<String, Product> inventory = new HashMap<>();
     //add scanner
+    Scanner myScanner = new Scanner(System.in);
 
     //add main
+    public static void main(String[] args) {
 
+    }
     //add loadinventory
 
     //add while true
