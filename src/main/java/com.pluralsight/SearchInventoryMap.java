15a22652e9ff4effadf3467596b968ba3494e0df
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-25 20:14:28 -0500

    added plans

--- src/main/java/com.pluralsight/SearchInventoryMap.java
+++ src/main/java/com.pluralsight/SearchInventoryMap.java
@@ -1,4 +1,16 @@
 package com.pluralsight;
 
+//add imports
+
 public class SearchInventoryMap {
+
+    //add hashmaps
+
+    //add scanner
+
+    //add main
+
+    //add loadinventory
+
+    //add while true
 }
